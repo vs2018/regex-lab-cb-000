bf95699af11a4af41c49dc9643a8c35263959b82
Vishal Sharma <vsh1819@gmail.com>
2018-06-12 12:49:49 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -10,11 +10,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  if text.scan(/^/).size != 0
-    return true
-  else
-    return false
-  end
+  words.scan(/^un/)
 end
 
 def words_five_letters_long(text)
