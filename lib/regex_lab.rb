c4378242fb02da8f9ebc432470996da7269f641b
Vishal Sharma <vsh1819@gmail.com>
2018-06-12 12:51:59 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -10,7 +10,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/\Aun$ing/)
+  text.scan(/\Aun\zing/)
 end
 
 def words_five_letters_long(text)
