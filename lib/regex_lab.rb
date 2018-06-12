152df3d5e81e1a58fb43f569a74a844e6b92dea2
Vishal Sharma <vsh1819@gmail.com>
2018-06-12 12:49:04 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,6 @@
 def starts_with_a_vowel?(word)
   
-  word.match(/[aeiouAEIOU]/) ? true: false
+  word.match(/^[aeiouAEIOU]/) ? true: false
   
   # if word.scan(/^[aeiouAEIOU]/).size != 0
   #   return true
