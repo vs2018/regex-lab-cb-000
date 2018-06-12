eb564613fecbca9a1013c2a14c10083fde7cfe8e
Vishal Sharma <vsh1819@gmail.com>
2018-06-12 13:07:50 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -14,7 +14,7 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  text.scan(/\B\w{5}\B/)
+  text.scan(/\B[a-zA-Z]{5}\B/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
