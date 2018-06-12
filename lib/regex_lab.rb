000a93cd0119788e9f5e5a58be7a91e8ac770e91
Vishal Sharma <vsh1819@gmail.com>
2018-06-12 13:10:39 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -19,7 +19,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/^[A-Z]\w$\W/)
+  text.match(/^[A-Z]\w$\W/) ? true: false
 end
 
 def valid_phone_number?(phone)
